9b83bbde9ba80e84047e27c356cc9229f58162f1
amb-ps <132484615+amb-ps@users.noreply.github.com>
2023-05-08 15:52:54 +0530

    route-2 changes to support filtering

--- js/route2.js
+++ js/route2.js
@@ -19,8 +19,24 @@ gridViewButton.onclick = function () {
 $(document).ready(function() {
     let appendInnerHtml='';
     for(let i=1;i<=40;i++){
-        appendInnerHtml += '<li class="listItem">List item '+ i+'</li>'
+        appendInnerHtml += '<li class="listItem" id='+i+'>List item '+ i+'</li>'
     }
     $('.list-view-filter').append(appendInnerHtml);
     $('#list-view-button').addClass('currentSelection')
 })
+
+function changeFunc(selection){
+  if(selection == 'lth'){
+    var elems = $('.list').children('li').remove();
+    elems.sort(function(a,b){
+        return parseInt(a.id) - parseInt(b.id);
+    });
+    $('.list').append(elems);
+  }else if(selection == 'htl'){
+    var elems = $('.list').children('li').remove();
+    elems.sort(function(a,b){
+        return parseInt(b.id) - parseInt(a.id);
+    });
+    $('.list').append(elems);
+  }
+}
